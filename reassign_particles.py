4bd86899e89b0d70e8105168a0ff41d331373dd8
Rafał Pietras <rafal.radx@gmail.com>
2024-05-10 12:09:02 +0200

    path update and motion corr removed

--- reassign_particles.py
+++ reassign_particles.py
@@ -20,15 +20,27 @@ def main(args: argparse.Namespace):
         # remove any leading UIDs in the path
         output_path = '_'.join(output_path.split('_')[1:])
         return output_path
-
+    
+    # {path: uid} in newly imported micrographs
     path_to_uid_map = {clean_path_to_match(path):exp_dset['uid'][idx] \
         for idx, path in enumerate(exp_dset['micrograph_blob/path'])}
+    # inverted map {uid:path}
+    uid_to_path_map = {v: k for k, v in path_to_uid_map.items()}
+
 
     for index, particle in enumerate(particle_dset.rows()):
         # get the micrograph path associated with the particle
         path_to_match = clean_path_to_match(particle['location/micrograph_path'])
+        # get new id from map based on micrograph path stored in particle data
+        new_uid = path_to_uid_map[path_to_match]
         # assign the uid to the particle dataset after lookup
-        particle_dset['location/micrograph_uid'][index] = path_to_uid_map[path_to_match]
+        particle_dset['location/micrograph_uid'][index] = new_uid
+        # update also a path to micrograph in particle data (add > in front)
+        particle_dset['location/micrograph_path'][index] = '>' + uid_to_path_map[new_uid]
+
+    # remove local motion correction description fields
+    motion = ['motion/type', 'motion/path', 'motion/idx','motion/frame_start', 'motion/frame_end', 'motion/zero_shift_frame', 'motion/psize_A']
+    particle_dset.drop_fields(motion)
 
     particle_dset.save(args.prtcls_path+"new_uuid")
 
